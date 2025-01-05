2a1951f951373adb4273295c024dac25532fbe8a
adarshem <adarshem@gmail.com>
2025-01-04 17:36:05 -0800

    Added optional catch all route

---
+++ app/archive/@archive/[[...filter]]/page.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Link from 'next/link';
+import NewsList from '@/components/NewsList';
+import {
+  getNewsForYear,
+  getAvailableNewsYears,
+  getAvailableNewsMonths,
+  getNewsForYearAndMonth,
+} from '@/lib/news';
+import { News } from '@/types/news';
+
+// https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes#optional-catch-all-segments
+
+export default function FilteredNewsPage({
+  params,
+}: {
+  params: { filter: string[] };
+}): React.JSX.Element {
+  const selectedYear = params.filter?.[0];
+  const selectedMonth = params.filter?.[1];
+  let news: News[] | undefined;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {news && news.length > 0 ? (
+        <NewsList news={news} />
+      ) : (
+        <p>
+          No news found for {selectedYear} {selectedMonth}
+        </p>
+      )}
+    </>
+  );
+}
