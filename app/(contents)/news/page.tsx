e1e2bc9539c18c1baca4fc15f908d6bea49f2488
adarshem <adarshem@gmail.com>
2025-01-08 17:17:58 -0800

    Fetch data from db from client side - useEffect

--- app/(contents)/news/page.tsx
+++ app/(contents)/news/page.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { DUMMY_NEWS } from '@/dummy-news';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import NewsList from '@/components/NewsList';
 
 export default function NewsPage(): React.JSX.Element {
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const resp = await fetch('http://localhost:8080/news');
+      if (!resp.ok) {
+        setError('Failed to fetch news');
+        setIsLoading(false);
+      }
+
+      const newsData = await resp.json();
+      setIsLoading(false);
+      setNews(newsData);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
     <div id="news">
       <h1>News</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {news && <NewsList news={news} />}
     </div>
   );
 }
