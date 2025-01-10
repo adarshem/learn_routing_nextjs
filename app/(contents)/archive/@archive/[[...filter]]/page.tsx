d822254290b82bb1119aabc609df36e749df1cfa
adarshem <adarshem@gmail.com>
2025-01-09 20:22:56 -0800

    React suspense

--- app/(contents)/archive/@archive/[[...filter]]/page.tsx
+++ app/(contents)/archive/@archive/[[...filter]]/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Link from 'next/link';
 import NewsList from '@/components/NewsList';
 import {
@@ -9,8 +9,78 @@ import {
 } from '@/lib/news';
 import { News } from '@/types/news';
 
-// https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes#optional-catch-all-segments
+async function FilterHeader({
+  year,
+  month,
+}: {
+  year: string;
+  month: string;
+}): Promise<React.JSX.Element> {
+  const availableYears = await getAvailableNewsYears();
+  let links = availableYears;
 
+  if (
+    (year && !availableYears.includes(year)) ||
+    (month && !getAvailableNewsMonths(year).includes(month))
+  ) {
+    throw new Error('Invalid filter.');
+  }
+
+  if (year && !month) {
+    links = getAvailableNewsMonths(year);
+  }
+
+  if (year && month) {
+    links = [];
+  }
+
+  return (
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {links.map((link) => {
+            const href = year ? `/archive/${year}/${link}` : `/archive/${link}`;
+            return (
+              <li key={link}>
+                <Link href={href}>{link}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+async function FilteredNews({
+  year,
+  month,
+}: {
+  year: string;
+  month: string;
+}): Promise<React.JSX.Element> {
+  let news: News[] | undefined;
+
+  if (year && !month) {
+    news = await getNewsForYear(year);
+  } else if (year && year) {
+    news = await getNewsForYearAndMonth(year, month);
+  }
+
+  return (
+    <>
+      {news && news.length > 0 ? (
+        <NewsList news={news} />
+      ) : (
+        <p>
+          No news found for {year} {month}
+        </p>
+      )}
+    </>
+  );
+}
+
+// https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes#optional-catch-all-segments
 export default async function FilteredNewsPage({
   params,
 }: {
@@ -18,53 +88,13 @@ export default async function FilteredNewsPage({
 }): Promise<React.JSX.Element> {
   const selectedYear = params.filter?.[0];
   const selectedMonth = params.filter?.[1];
-  let news: News[] | undefined;
-  let links = await getAvailableNewsYears();
-
-  if (selectedYear && !selectedMonth) {
-    news = await getNewsForYear(selectedYear);
-    links = getAvailableNewsMonths(selectedYear);
-  }
-
-  if (selectedYear && selectedMonth) {
-    news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
-    links = [];
-  }
-
-  if (
-    (selectedMonth &&
-      !(await getAvailableNewsYears()).includes(selectedYear)) ||
-    (selectedMonth &&
-      !getAvailableNewsMonths(selectedYear).includes(selectedMonth))
-  ) {
-    throw new Error('Invalid year or month');
-  }
 
   return (
     <>
-      <header id="archive-header">
-        <nav>
-          <ul>
-            {links.map((link) => {
-              const href = selectedYear
-                ? `/archive/${selectedYear}/${link}`
-                : `/archive/${link}`;
-              return (
-                <li key={link}>
-                  <Link href={href}>{link}</Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </header>
-      {news && news.length > 0 ? (
-        <NewsList news={news} />
-      ) : (
-        <p>
-          No news found for {selectedYear} {selectedMonth}
-        </p>
-      )}
+      <Suspense fallback={<p>Loading filtered news...</p>}>
+        <FilterHeader year={selectedYear} month={selectedMonth} />
+        <FilteredNews year={selectedYear} month={selectedMonth} />
+      </Suspense>
     </>
   );
 }
